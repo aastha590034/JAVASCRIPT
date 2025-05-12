0fa08be230f03952d41baba1a34e8f634e429869
AASTHA PRADHAN <aastha590034@gmail.com>
2025-05-12 17:11:02 +0530

    Update VARIABLES.js

--- .devcontainer/basics/VARIABLES.js
+++ .devcontainer/basics/VARIABLES.js
@@ -1,1 +1,9 @@
-
+var name="mummy"
+/*due to issues in function and global scope var use is not preffereed*/
+const accountId= 1234
+let accountName="suresh kumar"
+let accountEmail="reshsu@gmail.com"
+let accountState;
+accountcity="Raigarh"
+accountPassword="09876"
+console.table([accountId,accountName,accountEmail,accountState,accountCity,accountPassword])
